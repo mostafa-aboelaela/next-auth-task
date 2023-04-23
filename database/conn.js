1470917931e29139daec6f9ed08e7509bf35cb31
mostafa-aboelaela <aboelaela20@gmail.com>
2023-04-23 21:05:06 +0200

    changes

--- database/conn.js
+++ database/conn.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 
 const connectMongo = async()=> {
 	try {
-     const {connection} = await mongoose.connect(proscess.env.MONGO_URL);
+     const {connection} = await mongoose.connect(process.env.MONGO_URL);
 		 if(connection.readyState == 1){
 			  return Promise.resolve(true)
 		 }
